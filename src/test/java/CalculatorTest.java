bf43f26cce77e81173a9424fc8d3e8cf079dc52b
Lilia <liliag00@mail.ru>
2024-05-15 09:53:08 +0300

    change Calculator

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -3,29 +3,31 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    Calculator calculator=new Calculator();
 
     @Test
     void add() {
-        //TODO inser your realisation in test-method add
+        assertEquals(6,calculator.add(5,1));
     }
 
     @Test
     void dif() {
-        //TODO inser your realisation in test-method dif
+        //парам
+        assertEquals(3,calculator.dif(4,1));
     }
 
     @Test
     void div() {
-        //TODO inser your realisation in test-method div
+        assertEquals(6,calculator.div(6,1));
     }
 
     @Test
     void times() {
-        //TODO inser your realisation in test-method times
+        assertEquals(-1,calculator.times(-1,1));
     }
 
     @Test
     void solver() {
-        //TODO inser your realisation in test-method solver
+        assertEquals(7,calculator.solver());
     }
 }
